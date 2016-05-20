e247c172ac760cb25c22564642a8a7ee27b83be6
Lucas Possatti <lucas_possatti@hotmail.com>
2016-05-19 22:25:03 -0300

    Cria o modelo 'Projeto'

--- gestaoProjetos/models.py
+++ gestaoProjetos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Projeto(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField(blank=True)
+    inicio  = models.DateTimeField(auto_now_add=False)
+    fim = models.DateTimeField(auto_now_add=False)
+    progresso = models.FloatField()
+    linkGithub = models.URLField(blank=True)
+    linkGoogleDrive = models.URLField(blank=True)
+    linkSlack = models.URLField(blank=True)
+    linkTaiga = models.URLField(blank=True)
+    # cliente = models.ForeignKey('gestaoProjetos.EntidadeExterna', models.CASCADE)
+
+    def __str__(self):
+      return self.nome
